e221bed3bf57a303c07efb810c40630cd9fac1aa
Gianluca Cornacchia <c.luca74@gmail.com>
2016-11-28 16:56:27 +0100

    first version
    -updates spl,uboot,kernel,modules,rootfs,app
    
    tested on 0533 eval board

---
+++ create-update.sh
@@ -0,0 +1,191 @@
+#!/bin/sh
+#set -x
+ZIP_PASSWORD=""
+#"-P password"
+
+UBOOT_VERSION=0533-001
+SPL_VERSION=$UBOOT_VERSION
+KERNEL_VERSION=0533-001
+ROOTFS_VERSION=1.0
+ROOTFSLIVE_VERSION=0533-001
+
+HOME=$(pwd)
+OUTPUT=$HOME/output
+IMAGES=$HOME/images
+DEST=$HOME/usb-key
+ROOTFS_PKG=rootfs.tar.bz2
+KERNEL_PKG=kernel.tar.gz
+UBOOT_PKG=uboot.tar.gz
+KERNEL_BINARIES=$HOME/binaries/kernel/$KERNEL_VERSION
+KERNEL_LIVE_BINARIES=$HOME/binaries/kernel/$ROOTFSLIVE_VERSION-live
+UBOOT_BINARIES=$HOME/binaries/u-boot
+ROOTFS_BINARIES=$HOME/binaries/rootfs
+APP_PKG=app.tar.gz
+APP_BINARIES=$HOME/binaries/app
+
+MODULES_FILE=modules_$KERNEL_VERSION.tgz
+
+YOCTO_IMAGE=0533panelpcimx6q-$ROOTFS_VERSION
+
+skippartitioning=1
+skipuboot=0
+skipspl=0
+skipkernel=0
+skiprootfs=0
+
+usage() { echo "Usage: $0 [--no-uboot | --no-spl | --no-kernel | --no-rootfs | --makepartition | --help]" 1>&2; exit 1; }
+
+message() {
+	echo -e '\E[1;33m'$1'\E[0m'	
+}
+
+error() {
+	echo -e '\E[1;31m'$1'\E[0m'
+	echo " "	
+}
+
+TEMP=$(getopt -o 1 -l no-uboot,no-spl,no-kernel,no-rootfs,makepartition,help -- "$@")
+[ $? -eq 1 ] && exit
+
+eval set -- "$TEMP"
+
+while true
+do
+    case "$1" in
+        --no-uboot )  skipuboot=1; shift;;
+        --no-spl )    skipspl=1; shift;;
+        --no-kernel ) skipkernel=1; shift;;
+        --no-rootfs ) skiprootfs=1; shift;;
+        --makepartition ) skippartitioning=0; shift;;
+        --help )      usage; shift;;
+	    -- ) shift; break;;
+		* ) break;
+    esac
+done
+
+#create dirs
+rm -rf $DEST
+rm -rf $OUTPUT
+rm -rf tmp
+
+mkdir -p $DEST
+mkdir -p $OUTPUT
+mkdir tmp
+
+#cleanup
+#rm ./images/*.bgr 1>/dev/null 2>&1
+rm ./*.tar 1>/dev/null 2>&1
+rm ./*.gz 1>/dev/null 2>&1
+rm setup.sh 1>/dev/null 2>&1
+
+#--------------------------------------------------------------------------------------------------------
+#create your own graphics
+#each image must match the screen resolution and framebuffer format (rgb,bgr,rgb565,..)
+#--------------------------------------------------------------------------------------------------------
+#graphics
+message "Building graphics"
+avconv  -i $IMAGES/logo-updating.bmp -vcodec rawvideo -f rawvideo -pix_fmt bgr24 tmp/update-splash.bin 1>/dev/null 2>&1
+gzip < tmp/update-splash.bin > $OUTPUT/update-splash.gz
+avconv  -i $IMAGES/logo-update-terminated.bmp -vcodec rawvideo -f rawvideo -pix_fmt bgr24 tmp/update-terminated.bin 1>/dev/null 2>&1
+gzip < tmp/update-terminated.bin > $OUTPUT/update-terminated.gz
+#cp images/logo-boot.bmp $OUTPUT/logo.bmp
+
+#build u-boot+SPL update
+#-------------------
+cd tmp
+rm ./* 1>/dev/null 2>&1
+
+if [ $skipuboot = 0 ]; then
+	message "Adding u-boot"
+	cp $UBOOT_BINARIES/u-boot.img-$UBOOT_VERSION ./u-boot.img
+fi
+
+if [ $skipspl = 0 ]; then
+	message "Adding spl"
+	cp $UBOOT_BINARIES/SPL-$SPL_VERSION ./spl.img
+fi
+
+if [[ $skipspl = 0 || $skipuboot = 0 ]]; then
+	tar czvf $OUTPUT/$UBOOT_PKG *
+fi
+cd ..
+
+
+#build kernel update
+#-------------------
+if [ $skipkernel = 0 ]; then
+	message "Adding kernel"
+    filename_modules=$KERNEL_BINARIES/$MODULES_FILE
+    
+	if [ -e $filename_modules ]; then
+		message "Adding modules [/home/root/modules]"
+		
+		sudo rm -rf $APP_BINARIES/home/root/modules
+		sudo mkdir -p $APP_BINARIES/home/root/modules
+		cd $APP_BINARIES/home/root/modules
+		sudo tar xvf $filename_modules .
+		sudo chown -R root:root $APP_BINARIES/*
+		tar czvf $OUTPUT/$APP_PKG -C $APP_BINARIES .
+	fi
+	
+	#update kernel
+	cd $KERNEL_BINARIES
+	tar czvf $OUTPUT/$KERNEL_PKG zImage *.dtb
+	cd $HOME
+fi
+
+#build rootfs update
+#-------------------
+if [ $skiprootfs = 0 ]; then
+	message "Adding rootfs"
+	#update rootfs
+	if [ -f $ROOTFS_BINARIES/$YOCTO_IMAGE.ubi ]; then
+		#do nothing, ubi image doesn't require processing
+		ROOTFS_PKG=$ROOTFS_BINARIES/$YOCTO_IMAGE.ubi
+	elif [ -f $ROOTFS_BINARIES/$YOCTO_IMAGE.tar.bz2 ]; then
+		cp $ROOTFS_BINARIES/$YOCTO_IMAGE.tar.bz2 $OUTPUT/$ROOTFS_PKG
+	fi
+fi
+
+cd $HOME
+cp template-setup.sh $OUTPUT/setup.sh
+cd $OUTPUT
+
+#force make partition
+if [ $skippartitioning = 0 ]; then
+	sed -i 's/mkfs=0/mkfs=1/g' setup.sh
+fi
+
+#build update file
+message "Packaging files"
+zip -0 $ZIP_PASSWORD $DEST/update.bin setup.sh
+[ -f update-splash.gz ] && zip -0 $ZIP_PASSWORD $DEST/update.bin update-splash.gz
+[ -f update-terminated.gz ] && zip -0 $ZIP_PASSWORD $DEST/update.bin update-terminated.gz
+[ -f $KERNEL_PKG ] && zip -0 $ZIP_PASSWORD $DEST/update.bin $KERNEL_PKG
+[ -f $UBOOT_PKG ] && zip -0 $ZIP_PASSWORD $DEST/update.bin $UBOOT_PKG
+[ -f $ROOTFS_PKG ] && zip -0 $ZIP_PASSWORD $DEST/update2.bin $ROOTFS_PKG
+[ -f $APP_PKG ] && zip -0 $ZIP_PASSWORD $DEST/update3.bin $APP_PKG
+cd ..
+
+#copy live image
+if [ -f $KERNEL_LIVE_BINARIES/zImage ]; then
+	cp $KERNEL_LIVE_BINARIES/* $DEST
+else
+    error "Live image not found"
+fi
+
+cp $IMAGES/logo-updating.bmp $DEST/logo.bmp
+
+#cleanup
+rm -rf ./tmp
+rm -rf ./output
+
+echo
+echo -e '\E[1;37mUpdate package is stored in ./usb-key path'
+echo -e '\E[1;33mVersions: '
+[ $skipuboot = 0 ]  && echo 'U-Boot ' $UBOOT_VERSION
+[ $skipspl = 0 ]    && echo 'SPL    ' $SPL_VERSION
+[ $skipkernel = 0 ] && echo 'Kernel ' $KERNEL_VERSION
+[ $skiprootfs = 0 ] && echo 'Rootfs ' $ROOTFS_VERSION
+echo
+[ $skippartitioning = 0 ] && echo -e '\E[1;32m!!! Partitions will be formatted !!!'; echo;
---
+++ template-setup.sh
@@ -0,0 +1,180 @@
+
+message() {
+	echo "##### $1 #####"
+}
+
+error() {
+	echo "##### Error: $1 #####"
+}
+
+installPackage() {
+	#----------------
+	# Update package 
+	#----------------
+	echo 3 > /proc/sys/vm/drop_caches
+
+	if [ ! -f "$1" ]; then
+		error "Update package $1 not found, skipping..."
+		return
+	fi
+
+	#test archive first
+	gunzip -c $1 | tar t > /dev/null
+
+	if [ $? -ne 0 ]; then
+		error "Update package $1 corrupted! Skipping..."
+		return
+	fi
+
+	if [ ! -z $3 ]; then
+	if [ $3 == 'deleteAll' ]; then
+		message "Erasing $2..."
+		rm -rf $2/*
+	fi
+	fi
+
+	#apply updated files
+	tar xf $1 -C $2
+	rm $1
+}
+
+#must match the one used in create-update.sh
+ZIP_PASSWORD="password"
+source=$(dirname $0)
+#emmc | sdcard
+type=emmc
+mkfs=0
+mtd_spl=/dev/mtd0
+mtd_uboot=/dev/mtd1
+
+if [ $type = emmc ]; then
+	message "Updating EMMC"
+	dest_dev=mmcblk2
+elif [ $type = sdcard ]; then
+	message "Updating SDCARD"
+	dest_dev=mmcblk0
+fi
+
+dest_boot_partition=/run/media/$dest_dev'p1'
+dest_rootfs_partition=/run/media/$dest_dev'p2'
+dest_app_partition=/run/media/$dest_dev'p2'
+
+bootmedia=/run/media/mmcblk0p1
+[ ! -z "$(mount | grep sda)" ] && bootmedia=/run/media/sda
+[ ! -z "$(mount | grep sda1)" ] && bootmedia=/run/media/sda1
+
+cd $source
+
+#show splash screen
+zcat update-splash.gz > /dev/fb0
+
+#----------------------
+# partizionamento emmc:
+#Disk /dev/mmcblk2: , 3909091328 bytes
+#4 heads, 16 sectors/track, 119296 cylinders
+#Units = cylinders of 64 * 512 = 32768 bytes
+#
+#   Device Boot      Start         End      Blocks   Id  System
+#/dev/sdd1            2048       43007       20480    c  W95 FAT32 (LBA)
+#/dev/sdd2           43008     2140159     1048576   83  Linux
+#----------------------
+if [ $mkfs = 1 ]; then
+	message "Formatting..."
+	umount /dev/$dest_dev'p'*
+	
+	sleep 6
+	
+	echo -e "d\n3\nd\n2\nd\nn\np\n1\n\n+20M\nt\n0c\nn\np\n2\n\n+1G\nn\np\n3\n\n\nw\n" | fdisk /dev/$dest_dev
+
+	mkfs.vfat /dev/$dest_dev'p1'
+    if [ $? -ne 0 ]; then 
+        umount /dev/$dest_dev'p1'
+        mkfs.vfat /dev/$dest_dev'p1'
+    fi
+    
+	mkfs.ext4 /dev/$dest_dev'p2'
+    if [ $? -ne 0 ]; then 
+        umount /dev/$dest_dev'p2'
+        mkfs.ext4 /dev/$dest_dev'p2'
+    fi
+    
+	mkfs.ext4 /dev/$dest_dev'p3'
+    if [ $? -ne 0 ]; then 
+        umount /dev/$dest_dev'p3'
+        mkfs.ext4 /dev/$dest_dev'p3'
+    fi        
+    
+	udevadm trigger --action=add
+	udevadm settle --timeout=10
+fi
+
+#--------------
+# uboot
+#--------------
+if [ -f $source/uboot.tar.gz ]; then
+    message "Installing u-boot update -> $mtd_spl $mtd_uboot" 
+    
+    #rw nor
+    echo 90 > /sys/class/gpio/export
+    echo out > /sys/class/gpio/gpio90/direction
+    echo 1 > /sys/class/gpio/gpio90/value
+    
+	mkdir ./uboot
+	installPackage $source/uboot.tar.gz ./uboot
+
+	#update u-boot
+	flash_unlock $mtd_uboot
+	[ -f ./uboot/u-boot.img ] && flashcp ./uboot/u-boot.img  $mtd_uboot
+	flash_lock $mtd_uboot
+	
+	#update spl
+	if [ -f ./uboot/spl.img ]; then
+		echo Installing spl update...
+		flash_unlock $mtd_spl
+		dd if=$mtd_spl of=spl-header.bin bs=1024 count=1
+		cp spl-header.bin mtdspl.bin
+		cat ./uboot/spl.img >> mtdspl.bin
+		flashcp mtdspl.bin  $mtd_spl
+		flash_lock $mtd_spl
+	fi	
+	
+	#ro nor
+	echo 0 > /sys/class/gpio/gpio90/value
+fi
+
+#--------------
+# kernel
+#--------------
+message "Installing kernel update -> $dest_boot_partition"
+installPackage $source/kernel.tar.gz $dest_boot_partition
+
+#--------------
+# rootfs (bz2)
+#--------------
+if [ -f $bootmedia/update2.bin ]; then
+	message "Installing rootfs update -> $dest_rootfs_partition"
+	unzip -p -P $ZIP_PASSWORD $bootmedia/update2.bin | tar xjf - -C $dest_rootfs_partition
+fi
+
+#--------------
+# Application 
+#--------------
+if [ -f $bootmedia/update3.bin ]; then
+	message "Installing application update -> $dest_app_partition"
+	mkdir -p $dest_app_partition
+	unzip -p -P $ZIP_PASSWORD $bootmedia/update3.bin | tar xzf - -C $dest_app_partition
+fi
+
+cd /
+sync
+
+#not all devices are mounted
+umount /dev/sda
+umount /dev/sda1
+umount /dev/sda2
+umount /dev/$dest_dev'p'*
+
+#notify update is terminated
+zcat $source/update-terminated.gz > /dev/fb0
+
+
