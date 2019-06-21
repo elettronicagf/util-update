53fc3f9bd574e7f1fe436b0c878ae9720e88d09e
Gianluca Cornacchia <c.luca74@gmail.com>
2019-06-21 10:43:37 +0200

    aggiornato script create-binaries.sh

--- create-binaries.sh
+++ create-binaries.sh
@@ -12,3 +12,7 @@ cd rootfs
 ln -s /data2/developer/yocto_rootfs/0510mbugrfimx6-full-1.0.tar.bz2 .
 ln -s /data2/developer/yocto_rootfs/0510mbugrfimx6-full-2.0.tar.bz2 .
 ln -s /data2/developer/yocto_rootfs/0510mbugrfimx6-full-3.0.tar.bz2 .
+
+cd ../app
+ln -s ../../../app/recover/binaries/recover-1.0.tar.bz2 .
+ln -s ../../../app/recover/binaries/recover-1.1.tar.bz2 .
