389a6013e1b93007a6fe660e53d0c35037fd7384
Gianluca Cornacchia <c.luca74@gmail.com>
2019-07-16 09:35:03 +0200

    fix create-binaries.sh script

--- create-binaries.sh
+++ create-binaries.sh
@@ -9,10 +9,4 @@ ln -s ../../kernel/binaries/ kernel
 ln -s ../../u-boot/binaries/ u-boot
 
 cd rootfs
-ln -s /data2/developer/yocto_rootfs/0510mbugrfimx6-full-1.0.tar.bz2 .
-ln -s /data2/developer/yocto_rootfs/0510mbugrfimx6-full-2.0.tar.bz2 .
-ln -s /data2/developer/yocto_rootfs/0510mbugrfimx6-full-3.0.tar.bz2 .
-
-cd ../app
-ln -s ../../../app/recover/binaries/recover-1.0.tar.bz2 .
-ln -s ../../../app/recover/binaries/recover-1.1.tar.bz2 .
+ln -s /data2/developer/yocto_rootfs/0510smggrf*.tar.bz2 .
